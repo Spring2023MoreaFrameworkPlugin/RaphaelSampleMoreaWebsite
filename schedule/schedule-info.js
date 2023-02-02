f6962d82cc9cef6dfc8ee2171927fdf19d1ae7a3
RaphaelBumanlag <RaphaelBumanlag@users.noreply.github.com>
2023-02-02 22:23:26 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,7 @@
+moreaEventData = [
+  {title: "WS03: Kanji Reading and Writing Translation", url: "/RaphaelSampleMoreaWebsite/morea/lesson-6-week-2/experience-ws-3.html", start: "2023-01-13T23:55"},
+  {title: "Week 2: Japanese Grammar Notes 1-11", url: "/RaphaelSampleMoreaWebsite/modules/lesson-6-week-2", start: "2023-01-17", end: "2023-01-20T23:55"},
+  {title: "WS02: Kanji Reading and Writing Translation", url: "/RaphaelSampleMoreaWebsite/morea/lesson-6-week-1/experience-ws-2.html", start: "2023-01-13T23:55"},
+  {title: "Week 1: Orientation", url: "/RaphaelSampleMoreaWebsite/modules/lesson-6-week-1", start: "2023-01-09", end: "2023-01-13T23:55"},
+  {title: "WS01: Grammar Notes 1-3", url: "/RaphaelSampleMoreaWebsite/morea/lesson-6-week-1/experience-ws-1.html", start: "2023-01-13T23:55"}
+]
